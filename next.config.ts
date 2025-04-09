c472d94c7ca9afd1e52be3586dfdb52656474b71
Enkeym <julian1996@yandex.ru>
2025-04-09 09:34:59 +0300

    испарвил бал

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,31 @@
 import withBundleAnalyzer from "@next/bundle-analyzer"
 import type { NextConfig } from "next"
-import { createSecureHeaders } from "next-secure-headers"
 
-const isDev = process.env.NODE_ENV === "development"
 const isAnalyze = process.env.ANALYZE === "true"
 
-const secureHeaders = createSecureHeaders({
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: isDev
-        ? ["'self'", "'unsafe-inline'", "'unsafe-eval'"]
-        : ["'self'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "blob:"],
-      connectSrc: ["'self'", "https://api.emailjs.com"],
-      fontSrc: ["'self'"],
-      objectSrc: ["'none'"]
-    }
-  }
-})
+// const secureHeaders = createSecureHeaders({
+//   contentSecurityPolicy: {
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       scriptSrc: isDev
+//         ? ["'self'", "'unsafe-inline'", "'unsafe-eval'"]
+//         : ["'self'"],
+//       styleSrc: ["'self'", "'unsafe-inline'"],
+//       imgSrc: ["'self'", "data:", "blob:"],
+//       connectSrc: ["'self'", "https://api.emailjs.com"],
+//       fontSrc: ["'self'"],
+//       objectSrc: ["'none'"]
+//     }
+//   }
+// })
 
-const securityHeaders = [
-  ...secureHeaders,
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on"
-  }
-]
+// const securityHeaders = [
+//   ...secureHeaders,
+//   {
+//     key: "X-DNS-Prefetch-Control",
+//     value: "on"
+//   }
+// ]
 
 const baseConfig: NextConfig = {
   reactStrictMode: true,
@@ -50,26 +48,26 @@ const baseConfig: NextConfig = {
       transform: "three/src/{{member}}",
       skipDefaultConversion: true
     }
-  },
-
-  async headers() {
-    if (isDev) return []
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders
-      },
-      {
-        source: "/:all*(svg|avif|jpg|jpeg|png|webp|wasm)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable"
-          }
-        ]
-      }
-    ]
   }
+
+  // async headers() {
+  //   if (isDev) return []
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: securityHeaders
+  //     },
+  //     {
+  //       source: "/:all*(svg|avif|jpg|jpeg|png|webp|wasm)",
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=31536000, immutable"
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 }
 
 export default withBundleAnalyzer({ enabled: isAnalyze })(baseConfig)
