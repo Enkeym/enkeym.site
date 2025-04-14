c428fc5037ced674a63c1bad068cad55895715d8
Enkeym <julian1996@yandex.ru>
2025-04-14 10:36:00 +0300

    добавил бротли

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,25 +15,23 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
+        {/* Иконки */}
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.png" sizes="32x32" type="image/png" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           href="/apple-touch-icon.png"
           sizes="180x180"
         />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+
+        {/* PWA */}
         <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Preload только для LCP-ресурсов */}
         <link rel="preload" href="/foam.avif" as="image" fetchPriority="high" />
         <link rel="preload" href="/man.avif" as="image" fetchPriority="high" />
-        <link rel="preload" href="/og-image.jpg" as="image" />
-        <link
-          rel="preload"
-          href="/favicon.svg"
-          as="image"
-          type="image/svg+xml"
-        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
